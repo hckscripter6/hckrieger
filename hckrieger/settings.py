b37c0d39ca69cd50da57e262c72eaae4cb8b387f
Hunter Krieger <hunterk31@hotmail.com>
2020-01-09 20:27:34 -0500

    add allowed host

--- hckrieger/settings.py
+++ hckrieger/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'am6se8$icrafq2%oqvm2&*phvldp!^6yiyc4ui_132=+2s2gpk'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['hckrieger.herokuapp.com/']
 
 
 # Application definition
