f4df4a6bf6c941b44b12690abcf7526dfdefb808
Hunter Krieger <hunterk31@hotmail.com>
2020-01-09 21:59:02 -0500

    add to allowed host

--- hckrieger/settings.py
+++ hckrieger/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'am6se8$icrafq2%oqvm2&*phvldp!^6yiyc4ui_132=+2s2gpk'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'hckrieger.herokuapp.com']
 
 # Application definition
 
