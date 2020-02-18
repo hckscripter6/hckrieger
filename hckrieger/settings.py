8a89a421062a8541acb037f24ef5bd8964d47df6
Hunter Krieger <hunterk31@hotmail.com>
2020-02-17 20:21:11 -0500

    Change email config

--- hckrieger/settings.py
+++ hckrieger/settings.py
@@ -139,7 +139,7 @@ AWS_QUERYSTRING_AUTH = False
 
 
 EMAIL_BACKEND ='django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp-mail.outlook.com'
+EMAIL_HOST = 'smtp.live.com'
 EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASS')
 EMAIL_USE_TLS = True
