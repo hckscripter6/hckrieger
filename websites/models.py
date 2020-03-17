415465ba64174d3a4e84fdb1d78c8812c2f48475
Hunter Krieger <hunterk31@hotmail.com>
2020-03-17 18:19:31 -0400

    change styles

--- websites/models.py
+++ websites/models.py
@@ -3,9 +3,10 @@ from ckeditor.fields import RichTextField
 
 # Create your models here.
 class Website(models.Model):
+    organization = models.CharField(max_length=150, null=True)
     url = models.URLField(max_length=200)
     description = RichTextField()
     image = models.ImageField(upload_to="websites", null=True, blank=True)
     
     def __str__(self):
-        return self.url
+        return self.organization
