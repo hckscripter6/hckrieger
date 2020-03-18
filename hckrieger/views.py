fe0e331f255ba84f289514c0772ae9a737e50e3b
Hunter Krieger <hunterk31@hotmail.com>
2020-03-18 16:33:29 -0400

    add db interations to sites on homepage

--- hckrieger/views.py
+++ hckrieger/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
 from posts.models import Post
 from about.models import About
+from websites.models import Website
 
 def homepage(request):
     post1 = Post.objects.filter(published=True).last()
     post2_4 = Post.objects.order_by('-id').filter(published=True).all()[1:5]
-    return render(request, 'hckrieger/index.html', {'post1': post1, 'post2_4': post2_4})
+    sites = Website.objects.order_by('-id').all()
+    return render(request, 'hckrieger/index.html', {'post1': post1, 'post2_4': post2_4, 'sites': sites})
 
 def about(request):
     text = About.objects.first()
