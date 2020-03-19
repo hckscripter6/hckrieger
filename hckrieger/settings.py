24d2842dd34ac617e80aad793140628ac9cb3db4
Hunter Krieger <hunterk31@hotmail.com>
2020-03-18 21:20:07 -0400

    get rid of borders

--- hckrieger/settings.py
+++ hckrieger/settings.py
@@ -150,7 +150,7 @@ CKEDITOR_CONFIGS = {
 
 
 EMAIL_BACKEND ='django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.live.com'
+EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASS')
 EMAIL_USE_TLS = True
