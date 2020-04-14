b4f8cb14d820942164561e40eb4bd72a471d8687
Hunter Krieger <hunterk31@hotmail.com>
2020-04-14 17:25:42 -0400

    encrypt SK

--- hckrieger/settings.py
+++ hckrieger/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'am6se8$icrafq2%oqvm2&*phvldp!^6yiyc4ui_132=+2s2gpk'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
