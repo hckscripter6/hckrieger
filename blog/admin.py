63dd79e7a4251d95e5dacba8a3d8f4ca360bd60a
Hunter Krieger <hunterk31@hotmail.com>
2020-04-24 17:41:59 -0400

    auto-populate slug

--- blog/admin.py
+++ blog/admin.py
@@ -2,6 +2,11 @@ from django.contrib import admin
 from posts.models import Post, Category, Tag
 # Register your models here.
 
-admin.site.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    model = Post
+    list_display = ('title', 'subtitle',)
+    prepopulated_fields = {'slug': ('title',)}
+    
+admin.site.register(Post, PostAdmin)
 admin.site.register(Category)
 admin.site.register(Tag)
