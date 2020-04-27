29f1ef932316ee3cc3fb72545d0610d6fc49f20f
Hunter Krieger <hunterk31@hotmail.com>
2020-04-26 21:01:42 -0400

    but DEBUG in env var

--- hckrieger/settings.py
+++ hckrieger/settings.py
@@ -25,6 +25,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG')
+
 ALLOWED_HOSTS = ['hckrieger.herokuapp.com', 'localhost', 'www.hckrieger.com', '143.95.151.115', '127.0.0.1']
 
 # Application definition
