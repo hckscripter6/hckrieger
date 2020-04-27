934c033a7075346656a768d744b76ab9cac27bf8
Hunter Krieger <hunterk31@hotmail.com>
2020-04-26 21:08:01 -0400

    change env var DEBUG

--- hckrieger/settings.py
+++ hckrieger/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = os.environ.get('DEBUG')
 ALLOWED_HOSTS = ['hckrieger.herokuapp.com', 'localhost', 'www.hckrieger.com', '143.95.151.115', '127.0.0.1']
 
 # Application definition
