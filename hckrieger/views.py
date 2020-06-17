a38db86058ae77566d9a3ea66234354fdbf10937
Hunter Krieger <hunterk31@hotmail.com>
2020-06-17 16:52:16 -0400

    change around first post order

--- hckrieger/views.py
+++ hckrieger/views.py
@@ -4,7 +4,7 @@ from about.models import About
 from websites.models import Website
 
 def homepage(request):
-    post1 = Post.objects.filter(published=True).order_by('-date_published').last()
+    post1 = Post.objects.order_by('-date_published').filter(published=True).first()
     post2_4 = Post.objects.order_by('-date_published').filter(published=True).all()[1:5]
     sites = Website.objects.order_by('-id').all()
     return render(request, 'hckrieger/index.html', {'post1': post1, 'post2_4': post2_4, 'sites': sites})
